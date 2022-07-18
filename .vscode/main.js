ee4aeaf6e55d049ce98aa20a2f0e96bb74af605c
MahimanaGIT <mahimnabhatt@gmail.com>
2022-07-18 19:40:46 -0400

    First Commit

---
+++ .vscode/main.js
@@ -0,0 +1,7 @@
+/// <reference path="/home/deshmd2/.vscode/extensions/nur.script-0.1.2/global.d.ts" />
+// API - https://code.visualstudio.com/api/references/vscode-api 
+// 'Reload Window' or 'Restart Extension Host' after edit...
+
+Script.onActivate(_ctx => {
+	window.showInformationMessage('Hello World!');
+})
